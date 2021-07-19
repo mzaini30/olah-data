487d8c6196e6fdef966682a4da19c3f65b9ad3c2
mzaini30 <muhzaini30@gmail.com>
2021-07-19 15:42:14 +0800

    upload

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+	document.title = 'Olah Data'
+</script>
+
+<template>
+	<p><button>Open Data</button></p>
+	<p><button>Save Data</button></p>
+</template>
+
+<style scoped>
+	button {
+		width: 200px;
+		height: 100px;
+	}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {VitePWA} from 'vite-plugin-pwa'
 
-const namaAplikasi = 'Vite App'
+const namaAplikasi = 'Olah Data'
 
 // https://vitejs.dev/config/
 export default defineConfig({
