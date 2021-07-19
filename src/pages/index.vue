6f11b269faabb73b0fc047233cab7795eb2686c5
mzaini30 <muhzaini30@gmail.com>
2021-07-19 17:47:42 +0800

    upload

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,12 @@
 <script setup>
 	document.title = 'Olah Data'
 
+function download(filename, text) {  var element = document.createElement('a');  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));  element.setAttribute('download', filename);  element.style.display = 'none';  document.body.appendChild(element);  element.click();  document.body.removeChild(element);}
+
 	const nama = ref('sepeda')
 
 	function simpan(){
-		
+		download('data.txt', JSON.stringify(nama.value))
 	}
 </script>
 
@@ -13,8 +15,8 @@
 
 	<p>Masukkan nama:</p>
 	<input :value='nama' @input='x => nama = x.target.value'>
-	
-	<p><button>Open Data</button></p>
+
+	<p><input type='file'></p>
 	<p><button @click='simpan'>Save Data</button></p>
 </template>
 
