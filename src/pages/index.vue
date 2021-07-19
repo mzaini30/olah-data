8a92067174343765529a013b00bede01094c61cc
mzaini30 <muhzaini30@gmail.com>
2021-07-19 17:25:53 +0800

    upload

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,12 +2,20 @@
 	document.title = 'Olah Data'
 
 	const nama = ref('sepeda')
+
+	function simpan(){
+		
+	}
 </script>
 
 <template>
 	<p>Nama: {{nama}}</p>
+
+	<p>Masukkan nama:</p>
+	<input :value='nama' @input='x => nama = x.target.value'>
+	
 	<p><button>Open Data</button></p>
-	<p><button>Save Data</button></p>
+	<p><button @click='simpan'>Save Data</button></p>
 </template>
 
 <style scoped>
